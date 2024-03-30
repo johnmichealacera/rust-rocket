a8b19e22880cf1cc13e3ccccaf7ee15bd04d967e
JM Acera <john.acera@sportality.se>
2024-03-30 16:32:07 +0800

    use axum instead of rocket

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,46 @@
-use rocket::{get, routes};
+// use rocket::{get, routes};
 
-#[get("/<name>/<age>")]
-fn hello(name: &str, age: u8) -> String {
-    format!("Hello, {} year old named {}!", age, name)
-}
+// #[get("/<name>/<age>")]
+// fn hello(name: &str, age: u8) -> String {
+//     format!("Hello, {} year old named {}!", age, name)
+// }
 
-#[get("/jm")]
-fn helloJM() -> String {
-    format!("Hello JM my friend, ")
-}
+// #[get("/jm")]
+// fn hello_jm() -> String {
+//     format!("Hello JM my friend, ")
+// }
 
-#[rocket::main]
+// #[rocket::main]
+// async fn main() {
+//    let _ = rocket::build()
+//     .mount("/hello", routes![hello, hello_jm])
+//     .launch()
+//     .await;
+// }
+use axum::{
+    routing::{get, post},
+    http::StatusCode,
+    Json, Router,
+};
+use serde::{Deserialize, Serialize};
+
+#[tokio::main]
 async fn main() {
-   let _ = rocket::build().mount("/hello", routes![hello, helloJM]).launch().await;
+    // initialize tracing
+    tracing_subscriber::fmt::init();
+
+    // build our application with a route
+    let app = Router::new()
+        // `GET /` goes to `root`
+        .route("/", get(root));
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
+
+// basic handler that responds with a static string
+async fn root() -> &'static str {
+    "Hello, JM!"
+}
+
