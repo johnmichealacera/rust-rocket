1a65be092192584df48cb735732228f7e3e2bcbd
JM Acera <john.acera@sportality.se>
2024-03-31 14:53:59 +0800

    refactor code in breaking down duplicate code to separate function and code cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,15 @@
-use axum::http::{self};
-use axum::response::IntoResponse;
-use axum::routing::post;
 use axum::{
-    routing::get, Router,
-    extract::Json,
+    routing::get, routing::post, Router,
+    extract::Json, body::Body, http::{self, Response},
+    response::IntoResponse
 };
 use mongodb::{Client, options::ClientOptions, Database, error::Error};
 use dotenv::dotenv;
 use tokio::net::TcpListener;
-use std::env;
-use std::sync::{Arc, Mutex};
+use std::{
+    env, sync::{Arc, Mutex}
+};
 use serde_json::Value;
-use axum::extract::Path;
 use serde::{Deserialize, Serialize};
 
 #[tokio::main]
@@ -21,10 +19,8 @@ async fn main() {
     // build our application with a route
     let app = Router::new()
         .route("/", get(root))
-        .route("/user/:users", get(root_param))
-        .route("/json", post(create_introduction))
-        // .route("/json", post(insert_introductions))
-        .route("/jm", get(get_jm));
+        .route("/introduction", post(create_introduction))
+        .route("/introductions", get(get_introductions));
     let axum_address = env::var("AXUM_ADDRESS").expect("AXUM_ADDRESS must be set");
     let app_port = env::var("PORT").expect("PORT must be set");
     let axum_listener_address = format!("{}:{}", axum_address, app_port);
@@ -38,61 +34,37 @@ struct Introduction {
     icon: String,
 }
 
-async fn create_introduction(Json(payload): Json<Introduction>) -> impl IntoResponse {
-    println!("I am here");
-    println!("{:?}", payload);
-    // Create a new MongoConnection instance
-    let connection_result = MongoConnection::new().await;
-    match connection_result {
-        Ok(connection) => {
-            // Connection successful, use the connection object here
-            println!("Connected to MongoDB");
-            // Example usage: Get a handle to a database
-            let db = connection.db("personal");
-            // Retrieve all collection names in the database
-            match db.list_collection_names(None).await {
-                Ok(collections) => {
-                    println!("Available collections:");
-                    for collection_name in collections {
-                        println!("{}", collection_name);
-                    }
-                }
-                Err(err) => eprintln!("Error listing collections: {}", err),
-            }
+fn return_data(data: String) -> Response<Body> {
+    axum::http::Response::builder()
+        .status(http::StatusCode::OK)
+        .body(axum::body::Body::from(data))
+        .unwrap()
+}
+
+fn return_error(err: Error) -> Response<Body>{
+    eprintln!("Error finding documents: {}", err);
+    axum::http::Response::builder()
+        .status(http::StatusCode::INTERNAL_SERVER_ERROR)
+        .body("Error retrieving data".into())
+        .unwrap()
+}
+
+async fn create_introduction(Json(payload): Json<Introduction>) -> Response<Body> {
+    match connect_to_database().await {
+        Ok(db) => {
             match insert_introductions(&db, "introductions", payload).await {
                 Ok(()) => {
-                    axum::http::Response::builder()
-                        .status(http::StatusCode::OK)
-                        .body(axum::body::Body::from("Inserted successfully"))
-                        .unwrap()
+                    return_data(String::from("Inserted successfully"))
                 }
                 Err(err) => {
-                    // Handle error from find_all
-                    eprintln!("Error finding documents: {}", err);
-                    // Return an error response
-                    axum::http::Response::builder()
-                        .status(http::StatusCode::INTERNAL_SERVER_ERROR)
-                        .body("Error retrieving data".into())
-                        .unwrap()
+                    return_error(err)
                 }
             }
         }
         Err(e) => {
-            // Handle connection error
-            eprintln!("Error connecting to MongoDB: {}", e);
-            // Return an error response
-            axum::http::Response::builder()
-                .status(http::StatusCode::INTERNAL_SERVER_ERROR)
-                .body("Error connecting to MongoDB".into())
-                .unwrap()
+            return_error(e)
         }
     }
-}
-
-// basic handler that responds with a static string
-async fn root_param(Path(id): Path<u64>) -> String {
-    println!("{}", id);
-    format!("Hello, JM AAcera man param with ID: {}", id)
 }
 
 // basic handler that responds with a static string
@@ -100,56 +72,39 @@ async fn root() -> &'static str {
     "Hello, JM AAcera man!"
 }
 
-
-async fn get_jm() -> impl IntoResponse {
+async fn connect_to_database() -> Result<Database, mongodb::error::Error> {
     // Create a new MongoConnection instance
     let connection_result = MongoConnection::new().await;
     match connection_result {
         Ok(connection) => {
-            // Connection successful, use the connection object here
+            // Connection successful
             println!("Connected to MongoDB");
             // Example usage: Get a handle to a database
             let db = connection.db("personal");
-            // Retrieve all collection names in the database
-            match db.list_collection_names(None).await {
-                Ok(collections) => {
-                    println!("Available collections:");
-                    for collection_name in collections {
-                        println!("{}", collection_name);
-                    }
-                }
-                Err(err) => eprintln!("Error listing collections: {}", err),
-            }
-            match find_all(&db, "introductions").await {
-                Ok(data) => {
-                    // Process the data obtained from find_all
-                    // For demonstration, returning a success response with a message
-                    // Serialize the vector as JSON
-                    let json_data = serde_json::to_string(&data).unwrap();
-                    axum::http::Response::builder()
-                        .status(http::StatusCode::OK)
-                        .body(axum::body::Body::from(json_data))
-                        .unwrap()
-                }
-                Err(err) => {
-                    // Handle error from find_all
-                    eprintln!("Error finding documents: {}", err);
-                    // Return an error response
-                    axum::http::Response::builder()
-                        .status(http::StatusCode::INTERNAL_SERVER_ERROR)
-                        .body("Error retrieving data".into())
-                        .unwrap()
-                }
-            }
+            Ok(db)
         }
         Err(e) => {
             // Handle connection error
             eprintln!("Error connecting to MongoDB: {}", e);
-            // Return an error response
-            axum::http::Response::builder()
-                .status(http::StatusCode::INTERNAL_SERVER_ERROR)
-                .body("Error connecting to MongoDB".into())
-                .unwrap()
+            Err(e)
+        }
+    }
+}
+
+async fn get_introductions() -> impl IntoResponse {
+    match connect_to_database().await {
+        Ok(database) => {
+            match find_all(&database, "introductions").await {
+                Ok(data) => {
+                    return_data(data)
+                }
+                Err(err) => {
+                    return_error(err)
+                }
+            }
+        }
+        Err(e) => {
+            return_error(e)
         }
     }
 }
@@ -160,12 +115,11 @@ async fn insert_introductions(db: &Database, collection_name: &str, data: Introd
     Ok(())
 }
 
-async fn find_all(db: &Database, collection_name: &str) -> Result<Vec<Value>, mongodb::error::Error> {
+async fn find_all(db: &Database, collection_name: &str) -> Result<String, mongodb::error::Error> {
     let collection: mongodb::Collection<Value> = db.collection(collection_name);
     let mut cursor = collection.find(None, None).await?;
     let mut documents = Vec::new();
     while let Ok(result) = cursor.advance().await {
-        println!("{}", result);
         match result {
             true => {
                 match cursor.deserialize_current() {
@@ -176,12 +130,10 @@ async fn find_all(db: &Database, collection_name: &str) -> Result<Vec<Value>, mo
             false => break
         }
     }
-
-    println!("{:?}", documents);
-
-    println!("this is the end of loop");
-    // Process the cursor...
-    Ok(documents)
+    // Process the data obtained from find_all
+    // Serialize the vector as JSON
+    let json_data = serde_json::to_string(&documents).unwrap();
+    Ok(json_data)
 }
 
 
